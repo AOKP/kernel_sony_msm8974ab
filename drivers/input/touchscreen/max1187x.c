277b0e79e8800bf8b9227a0fe8123fc7d7475171
Shane Francis <bigbeeshane@gmail.com>
2014-07-06 17:27:22 +0100

    driver: max1187x: implement standard android suspend/resume
    Change-Id: Ia3b15f404a7f6bd204302246c4ffc9c1415ff521

--- drivers/input/touchscreen/max1187x.c
+++ drivers/input/touchscreen/max1187x.c
@@ -37,6 +37,10 @@
 #include <linux/regulator/consumer.h>
 #include <linux/slab.h>
 #include <linux/types.h>
+#ifdef CONFIG_FB
+#include <linux/fb.h>
+#include <linux/notifier.h>
+#endif
 
 #define NWORDS(a)    (sizeof(a) / sizeof(u16))
 #define BYTE_SIZE(a) ((a) * sizeof(u16))
@@ -279,6 +283,11 @@ struct data {
 	struct input_dev *input_dev;
 	char phys[32];
 	struct device_node *evdt_node;
+#ifdef CONFIG_FB
+	struct notifier_block fb_notif;
+	struct work_struct notify_resume;
+	struct work_struct notify_suspend;
+#endif
 	bool is_suspended;
 	bool pm_suspended;
 	bool irq_on_suspend;
@@ -326,6 +335,12 @@ struct data {
 
 	unsigned long ew_timeout;
 };
+
+#ifdef CONFIG_FB
+static int fb_notifier_callback(struct notifier_block *self, unsigned long event, void *data);
+static void notify_resume(struct work_struct *work);
+static void notify_suspend(struct work_struct *work);
+#endif
 
 static int vreg_configure(struct data *ts, bool enable);
 
@@ -2325,6 +2340,18 @@ static int probe(struct i2c_client *client, const struct i2c_device_id *id)
 	if (ret)
 		dev_warn(dev, "No firmware response (%d)", ret);
 
+	/* configure suspend/resume */
+#ifdef CONFIG_FB
+	ts->fb_notif.notifier_call = fb_notifier_callback;
+	ret = fb_register_client(&ts->fb_notif);
+	if (ret) {
+		dev_err(dev, "Unable to register fb_notifier");
+	} else {
+		INIT_WORK(&ts->notify_resume, notify_resume);
+		INIT_WORK(&ts->notify_suspend, notify_suspend);
+	}
+#endif
+
 	ts->is_suspended = false;
 	ts->pm_suspended = false;
 	ts->irq_on_suspend = false;
@@ -2355,6 +2382,9 @@ static int probe(struct i2c_client *client, const struct i2c_device_id *id)
 
 err_device_init_sysfs_remove_group:
 	remove_sysfs_entries(ts);
+#ifdef CONFIG_FB
+	fb_unregister_client(&ts->fb_notif);
+#endif
 err_device_init_irq:
 	input_unregister_device(ts->input_dev);
 err_device_init_inputdev:
@@ -2373,6 +2403,10 @@ static void shutdown(struct i2c_client *client)
 {
 	struct data *ts = i2c_get_clientdata(client);
 
+#ifdef CONFIG_FB
+	struct device *dev = &client->dev;
+#endif
+
 	if (ts == NULL)
 		return;
 
@@ -2385,6 +2419,13 @@ static void shutdown(struct i2c_client *client)
 
 	if (ts->sysfs_created && ts->sysfs_created--)
 		device_remove_bin_file(&client->dev, &dev_attr_report);
+
+#ifdef CONFIG_FB
+	if (fb_unregister_client(&ts->fb_notif))
+		dev_err(dev, "Error occurred while unregistering fb_notifier.");
+	cancel_work_sync(&ts->notify_resume);
+	cancel_work_sync(&ts->notify_suspend);
+#endif
 
 	if (client->irq)
 		free_irq(client->irq, ts);
@@ -2616,6 +2657,49 @@ static int suspend_noirq(struct device *dev)
 
 	return 0;
 }
+
+#ifdef CONFIG_FB
+static void notify_resume(struct work_struct *work)
+{
+	struct data *ts  = container_of(work, struct data, notify_resume);
+
+	if (ts->is_suspended)
+		set_resume_mode(ts);
+}
+
+static void notify_suspend(struct work_struct *work)
+{
+	struct data *ts  = container_of(work, struct data, notify_suspend);
+
+	if (!ts->is_suspended)
+		set_suspend_mode(ts);
+}
+
+static int fb_notifier_callback(struct notifier_block *self,
+				unsigned long event, void *data)
+{
+	struct fb_event *evdata = data;
+	int *blank;
+	struct data *ts = container_of(self, struct data, fb_notif);
+
+	if (evdata && evdata->data && event == FB_EVENT_BLANK && ts &&
+			ts->client) {
+		blank = evdata->data;
+		if (*blank != FB_BLANK_UNBLANK) {
+			dev_dbg(&ts->client->dev, "FB_BLANK_BLANKED\n");
+			cancel_work_sync(&ts->notify_resume);
+			cancel_work_sync(&ts->notify_suspend);
+			schedule_work(&ts->notify_suspend);
+		} else if (*blank == FB_BLANK_UNBLANK) {
+			dev_dbg(&ts->client->dev, "FB_BLANK_UNBLANK\n");
+			cancel_work_sync(&ts->notify_suspend);
+			cancel_work_sync(&ts->notify_resume);
+			schedule_work(&ts->notify_resume);
+		}
+	}
+	return 0;
+}
+#endif
 
 static int suspend(struct device *dev)
 {
